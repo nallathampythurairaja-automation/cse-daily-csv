d8d64e98f1c7e5c50d5aad795b8428c2b7859ee8
nallathampythurairaja-automation <nallathampythurairaja@gmail.com>
2026-02-16 14:47:07 +0530

    Update API URL for detailed trades

--- download_api2.py
+++ download_api2.py
@@ -4,7 +4,7 @@ import requests
 import pandas as pd
 
 OUT_DIR = "data"
-API_URL = "https://www.cse.lk/equity/detailed-trades"
+API_URL = "https://www.cse.lk/api/detailedTrades"
 
 def main():
     os.makedirs(OUT_DIR, exist_ok=True)
